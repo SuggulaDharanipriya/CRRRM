8fe7c12ac00d2de1ef88e4075bc2969b0290b5f5
SuggulaDharanipriya <sugguladharanipriya27@gmail.com>
2024-11-16 15:25:38 +0530

    Your commit message here

---
+++ Customer-Relationship-Management-App-master/accounts/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('login/', views.loginPage,name='login'),
+    path('Forget/', views.Forget,name='Forget'),
+    path('product/', views.product,name='product'),
+    path('register/', views.registerPage, name='register'),
+    path('product/', views.product, name='product'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('createorder/<str:pk>/', views.createOrder, name='createorder'),
+    path('updateorder/<str:pk>/', views.updateOrder, name='updateorder'),
+    path('deleteorder/<str:pk>/', views.deleteOrder, name='deleteorder'),
+
+]
